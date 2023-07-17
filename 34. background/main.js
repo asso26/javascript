7b854e39e004ed2b078b9a079767fee87d5e7b30
asso26 <139745453+asso26@users.noreply.github.com>
2023-07-17 16:18:03 +0900

    Add files via upload

---
+++ 34. background/main.js
@@ -0,0 +1,22 @@
+let docwidth=window.innerWidth;//화면의 넓이값
+let wrap=document.querySelector('.newspaperWrap');
+let imgs=document.querySelectorAll('.newspaperWrap .hb');
+let slideWidth=wrap.clientWidth;//wrap의 넓이값
+
+
+wrap.addEventListener('mousemove',function(e){
+    let mouseX=e.clientX;
+    console.log(mouseX)
+    let offset=(mouseX/docwidth)*slideWidth - mouseX/2;
+
+    for(let i=0; i<imgs.length; i++){
+        imgs[i].style.transform=`translateX(${-offset}px)`;
+    }
+})
+
+wrap.addEventListener('mouseleave',function(){
+    for(let i=0; i<imgs.length; i++){
+        imgs[i].style.transform=`translateX(0px)`;
+        imgs[i].style.transition='0.5s';
+    } 
+})
